ee14f15d5d708d196a16499e9add4af83ed25232
Mehul Mohagaonkar <memochamp96@gmail.com>
2017-01-24 18:37:42 -0500

    Person1 : Mehul commit

--- src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Mehul Mohagaonkar
  *  @version 1.1
  */
 public class Person1 {
@@ -30,9 +30,14 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 1 put your implementation here
-	  return null;
-	}
+        String output = "";
+        for (int i = 2; i < input.length(); i++) {
+            output += input.charAt(i);
+        }
+        output += input.charAt(0);
+        output += input.charAt(1);
+        return output;
+    }
 	
 	/**
 	 * Return a string rep of this object
