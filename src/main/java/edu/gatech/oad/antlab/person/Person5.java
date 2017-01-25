d5e34911e3555ab50740a52df9842416d549f86c
Vivekanand Rajasekar <vivekraja07@lawn-128-61-56-229.lawn.gatech.edu>
2017-01-24 21:56:47 -0500

    added a person five

--- src/main/java/edu/gatech/oad/antlab/person/Person5.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person5.java
@@ -3,8 +3,8 @@ package edu.gatech.oad.antlab.person;
 /**
  *  A simple class for person 5
  *  returns their name and a
- *  modified string 
- *  
+ *  modified string
+ *
  *  @author Bob
  *  @version 1.1
  */
@@ -31,19 +31,28 @@ public class Person5 {
 	 */
 	private String calc(String input) {
 	  //Person 5 put your implementation here
-	  return null;
+		input+=input.charAt(input.length-1);
+		input=input.substring(1);
+		input+=input.charAt(input.length-1);
+		input=input.substring(1);
+	    return input;
 	}
-	
+
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {
 	  return name + calc(input);
 	}
+
+	public static void main(String[] args) {
+        Person5 test = new Person5("1");
+        System.out.println(test.calc("gtg123b"));
+    }
 
 }
